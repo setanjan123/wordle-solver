e24eeb5cb4a34d99c298c46af3c83edce636461f
Setanjan Roy <setanjan123@gmail.com>
2022-04-03 14:09:35 +0530

    Implementing hint system

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   const messageInfo = JSON.parse(message.info).solution
   chrome.storage.local.set({'wordle-solution':messageInfo},()=>{})
+  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+messageInfo)
+  .then(res=>res.json())
+  .then(res=>{
+       if(res.title=='No Definitions Found')  chrome.storage.local.set({'wordle-hint':'No Hints available'},()=>{})
+       else {
+           const meanings = res[0].meanings
+           const meaning = meanings[Math.floor(Math.random()*meanings.length)]
+           const definition = meaning.definitions[Math.floor(Math.random()*meaning.definitions.length)].definition
+           chrome.storage.local.set({'wordle-hint':definition},()=>{})
+       }
+  })
 });
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 document.getElementById("solve").addEventListener("click", async () => {
     chrome.storage.local.get('wordle-solution', function (result) {
-        document.getElementById("solution").innerHTML =  '<b>'+result['wordle-solution']+'<b>'
+        document.getElementById("solution").innerHTML =  'The Solution is: <b>'+result['wordle-solution']+'<b>'
     });
 });
+
+document.getElementById("hintbutton").addEventListener("click", async () => {
+    chrome.storage.local.get('wordle-hint', function (result) {
+        console.log(result)
+        document.getElementById("hint").innerHTML =  '<b>'+result['wordle-hint']+'<b>'
+    });
+});
+
